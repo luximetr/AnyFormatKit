f1337e26c5d9b4d16a5bb6c838342c3582572580
BRANDERSTUDIO <alexandr.orlov@brander.ua>
2017-11-15 08:18:34 +0200

    Changed simulator name in run-tests script.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xcodebuild -workspace ../AnyFormatKit.xcworkspace -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=11.1' test
+xcodebuild -workspace ../AnyFormatKit.xcworkspace -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.1' test
