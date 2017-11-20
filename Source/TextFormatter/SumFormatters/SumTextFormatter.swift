b1d6e421160cb8f2f272846f911a963895950ab5
BRANDERSTUDIO <luximetr.notification@gmail.com>
2017-11-20 13:34:02 +0200

    Added tests for US and European currency formats.

--- Source/TextFormatter/SumFormatters/SumTextFormatter.swift
+++ Source/TextFormatter/SumFormatters/SumTextFormatter.swift
@@ -9,219 +9,218 @@
 import Foundation
 
 public class SumTextFormatter: TextFormatterProtocol {
-    
-    private static let maxIntSize = String(Int.max).count - 1
-    
-    public var maximumDecimalCharacters: Int = 2
-    public var maximumIntegerCharacters: Int = 14 {
-        didSet {
-            if maximumIntegerCharacters > SumTextFormatter.maxIntSize { maximumIntegerCharacters = SumTextFormatter.maxIntSize }
-        }
+  private static let maxIntSize = String(Int.max).count - 1
+  
+  public var maximumDecimalCharacters: Int = 2
+  public var maximumIntegerCharacters: Int = 14 {
+    didSet {
+      if maximumIntegerCharacters > SumTextFormatter.maxIntSize { maximumIntegerCharacters = SumTextFormatter.maxIntSize }
     }
-    
-    var prefixStr: String?
-    var suffixStr: String?
-    var decimalSeparator: String = "."
-    var groupingSeparator: String = ","
-    var numberOfCharactersInGroup = 3
-    
-    /*
-     A mask, used for formatting input data. Can has a prefix (optional),
-                                             an integer part with separator,
-                                             a decimal part with separator(optional)
-                                             and a suffix(optional)
-     Examples (special symbol == "#"):
-        "Suf #,###.# $" -> "Suf 1,234,567.12 $"
-        "# ##$" -> "12 34 56$"
-        "# ###.#" -> "123 456.78"
-     */
-    var textPattern: String
-    
-    // Symbol that will be replace by input symbols
-    var specialSymbol: Character
-
-    private let possibleDividers = CharacterSet(charactersIn: ".,")
-
-    public init(textPattern: String, specialSymbol: Character = "#") {
-        self.textPattern = textPattern
-        self.specialSymbol = specialSymbol
-        extractDataFromPattern()
+  }
+  
+  var prefixStr: String?
+  var suffixStr: String?
+  var decimalSeparator: String = "."
+  var groupingSeparator: String = ","
+  var numberOfCharactersInGroup = 3
+  
+  /*
+   A mask, used for formatting input data. Can has a prefix (optional),
+   an integer part with separator,
+   a decimal part with separator(optional)
+   and a suffix(optional)
+   Examples (special symbol == "#"):
+   "Suf #,###.# $" -> "Suf 1,234,567.12 $"
+   "# ##$" -> "12 34 56$"
+   "# ###.#" -> "123 456.78"
+   */
+  var textPattern: String
+  
+  // Symbol that will be replace by input symbols
+  var specialSymbol: Character
+  
+  private let possibleDividers = CharacterSet(charactersIn: ".,")
+  
+  public init(textPattern: String, specialSymbol: Character = "#") {
+    self.textPattern = textPattern
+    self.specialSymbol = specialSymbol
+    extractDataFromPattern()
+  }
+  
+  private func extractDataFromPattern() {
+    self.suffixStr = getSuffix(from: textPattern, specialSymbol: specialSymbol)
+    self.prefixStr = getPrefix(from: textPattern, specialSymbol: specialSymbol)
+    self.decimalSeparator = getDecimalSeparator(from: textPattern, specialSymbol: specialSymbol)
+    self.groupingSeparator = getGroupingSeparator(from: textPattern, specialSymbol: specialSymbol)
+    self.numberOfCharactersInGroup = getNumberOfCharactersInGroup(from: textPattern, specialSymbol: specialSymbol)
+  }
+  
+  private func getPrefix(from pattern: String, specialSymbol: Character) -> String {
+    var result = ""
+    for i in pattern.enumerated() {
+      if i.element == specialSymbol {
+        let index = pattern.index(pattern.startIndex, offsetBy: i.offset)
+        result = String(pattern[pattern.startIndex..<index])
+        break
+      }
     }
-    
-    private func extractDataFromPattern() {
-        self.suffixStr = getSuffix(from: textPattern, specialSymbol: specialSymbol)
-        self.prefixStr = getPrefix(from: textPattern, specialSymbol: specialSymbol)
-        self.decimalSeparator = getDecimalSeparator(from: textPattern, specialSymbol: specialSymbol)
-        self.groupingSeparator = getGroupingSeparator(from: textPattern, specialSymbol: specialSymbol)
-        self.numberOfCharactersInGroup = getNumberOfCharactersInGroup(from: textPattern, specialSymbol: specialSymbol)
+    return result
+  }
+  
+  private func getSuffix(from pattern: String, specialSymbol: Character) -> String {
+    var result = ""
+    var i = pattern.count
+    while (i > 0) {
+      i -= 1
+      if let currentChar = pattern.characterAt(i), currentChar == specialSymbol {
+        let index = pattern.index(pattern.endIndex, offsetBy: -(pattern.count - i - 1))
+        result = String(pattern[index..<pattern.endIndex])
+        break
+      }
     }
-    
-    private func getPrefix(from pattern: String, specialSymbol: Character) -> String {
-        var result = ""
-        for i in pattern.enumerated() {
-            if i.element == specialSymbol {
-                let index = pattern.index(pattern.startIndex, offsetBy: i.offset)
-                result = String(pattern[pattern.startIndex..<index])
-                break
-            }
-        }
-        return result
-    }
-    
-    private func getSuffix(from pattern: String, specialSymbol: Character) -> String {
-        var result = ""
-        var i = pattern.count
-        while (i > 0) {
-            i -= 1
-            if let currentChar = pattern.characterAt(i), currentChar == specialSymbol {
-                let index = pattern.index(pattern.endIndex, offsetBy: -(pattern.count - i - 1))
-                result = String(pattern[index..<pattern.endIndex])
-                break
-            }
-        }
-        return result
-    }
-    
-    private func getDecimalSeparator(from pattern: String, specialSymbol: Character) -> String {
-        var result = ""
-        var i = pattern.count - (suffixStr?.count ?? 0)
-        while (i > 0) {
-            i -= 1
-            if let currentChar = pattern.characterAt(i), currentChar != specialSymbol {
-                var tmpResult = ""
-                var j = i
-                
-                while (pattern.characterAt(j) != specialSymbol) {
-                    guard let char = pattern.characterAt(j) else { break }
-                    tmpResult.append(char)
-                    j -= 1
-                }
-                
-                result = String(tmpResult.reversed())
-                break
-            }
-        }
-        return result
-    }
-    
-    private func getGroupingSeparator(from pattern: String, specialSymbol: Character) -> String {
-        var result = ""
-        var i = prefixStr?.count ?? 0
-        while (i < pattern.count) {
-            if let currentChar = pattern.characterAt(i), currentChar != specialSymbol {
-                var j = i
-                while (pattern.characterAt(j) != specialSymbol) {
-                    guard let char = pattern.characterAt(j) else { break }
-                    result.append(char)
-                    j += 1
-                }
-                break
-            }
-            i+=1
-        }
-        return result
-    }
-    
-    private func getNumberOfCharactersInGroup(from pattern: String, specialSymbol: Character) -> Int {
-        var result = 0
-        var i = prefixStr?.count ?? 0
-        while (i < pattern.count) {
-            if let currentChar = pattern.characterAt(i), currentChar.description == groupingSeparator {
-                var j = i + 1
-                while (pattern.characterAt(j) == specialSymbol && j < pattern.count) {
-                    result += 1
-                    j += 1
-                }
-                break
-            }
-            i+=1
-        }
-        return result
-    }
-    
-    func isRequireSubstitute(symbol: Character) -> Bool {
-        return symbol == specialSymbol
-    }
-    
-    private func stringForDecimal(decimal: Int) -> String {
-        var result = ""
-        let reversString = String(String(decimal).reversed())
-        let isNegative = decimal < 0
-        var reversLenght = reversString.count
-        if isNegative {
-            // for minus symbol
-            reversLenght -= 1
-        }
-        for i in 0..<reversLenght {
-            guard let currentChar = reversString.characterAt(i) else { continue }
-            result.append(currentChar)
-            if i % numberOfCharactersInGroup == (numberOfCharactersInGroup - 1), i != 0, i != reversLenght - 1 {
-                result.append(groupingSeparator)
-            }
-        }
-        if isNegative {
-            result.append("-")
-        }
-        return String(result.reversed())
-    }
-    
-    
-    public func formattedText(from unformatted: String?) -> String? {
+    return result
+  }
+  
+  private func getDecimalSeparator(from pattern: String, specialSymbol: Character) -> String {
+    var result = ""
+    var i = pattern.count - (suffixStr?.count ?? 0)
+    while (i > 0) {
+      i -= 1
+      if let currentChar = pattern.characterAt(i), currentChar != specialSymbol {
+        var tmpResult = ""
+        var j = i
         
-        guard let unformattedString = unformatted else { return nil }
-        guard !(unformattedString.isEmpty) else { return "\(prefixStr ?? "")\(suffixStr ?? "")" }
-        
-        var resultString = ""
-        var hasIncompletedDecimal = false
-
-        resultString.append(prefixStr ?? "")
-        
-        var doubleStringValue = unformattedString.components(separatedBy: possibleDividers).joined(separator: decimalSeparator)
-        if doubleStringValue.characterAt(0) == Character(decimalSeparator) {
-          doubleStringValue.insert("0", at: doubleStringValue.startIndex)
+        while (pattern.characterAt(j) != specialSymbol) {
+          guard let char = pattern.characterAt(j) else { break }
+          tmpResult.append(char)
+          j -= 1
         }
         
-        if doubleStringValue.characterAt(doubleStringValue.count - 1) == Character(decimalSeparator) {
-            doubleStringValue.append("0")
-            hasIncompletedDecimal = true
+        result = String(tmpResult.reversed())
+        break
+      }
+    }
+    return result
+  }
+  
+  private func getGroupingSeparator(from pattern: String, specialSymbol: Character) -> String {
+    var result = ""
+    var i = prefixStr?.count ?? 0
+    while (i < pattern.count) {
+      if let currentChar = pattern.characterAt(i), currentChar != specialSymbol {
+        var j = i
+        while (pattern.characterAt(j) != specialSymbol) {
+          guard let char = pattern.characterAt(j) else { break }
+          result.append(char)
+          j += 1
         }
-        
-        guard let doubleValue = Double(doubleStringValue) else { return unformattedString }
-
-        let decimalValue = Int(doubleValue)
-        resultString.append(stringForDecimal(decimal: decimalValue))
-
-        if doubleStringValue.contains(decimalSeparator) {
-          resultString.append(decimalSeparator)
-          if doubleStringValue.characterAt(doubleStringValue.count - 1) != Character(decimalSeparator) {
-            let floatString = doubleStringValue.components(separatedBy: decimalSeparator)[1]
-            resultString.append(correctedDecimalPart(from: floatString))
-            
-          }
+        break
+      }
+      i+=1
+    }
+    return result
+  }
+  
+  private func getNumberOfCharactersInGroup(from pattern: String, specialSymbol: Character) -> Int {
+    var result = 0
+    var i = prefixStr?.count ?? 0
+    while (i < pattern.count) {
+      if let currentChar = pattern.characterAt(i), currentChar.description == groupingSeparator {
+        var j = i + 1
+        while (pattern.characterAt(j) == specialSymbol && j < pattern.count) {
+          result += 1
+          j += 1
         }
-        
-        if hasIncompletedDecimal { resultString.removeLast() }
-        resultString.append(suffixStr ?? "")
-        return resultString
+        break
+      }
+      i+=1
+    }
+    return result
+  }
+  
+  func isRequireSubstitute(symbol: Character) -> Bool {
+    return symbol == specialSymbol
+  }
+  
+  private func stringForDecimal(decimal: Int) -> String {
+    var result = ""
+    let reversString = String(String(decimal).reversed())
+    let isNegative = decimal < 0
+    var reversLenght = reversString.count
+    if isNegative {
+      // for minus symbol
+      reversLenght -= 1
+    }
+    for i in 0..<reversLenght {
+      guard let currentChar = reversString.characterAt(i) else { continue }
+      result.append(currentChar)
+      if i % numberOfCharactersInGroup == (numberOfCharactersInGroup - 1), i != 0, i != reversLenght - 1 {
+        result.append(groupingSeparator)
+      }
+    }
+    if isNegative {
+      result.append("-")
+    }
+    return String(result.reversed())
+  }
+  
+  
+  public func formattedText(from unformatted: String?) -> String? {
+    
+    guard let unformattedString = unformatted else { return nil }
+    guard !(unformattedString.isEmpty) else { return "\(prefixStr ?? "")\(suffixStr ?? "")" }
+    
+    var resultString = ""
+    var hasIncompletedDecimal = false
+    
+    resultString.append(prefixStr ?? "")
+    
+    var doubleStringValue = unformattedString.components(separatedBy: possibleDividers).joined(separator: decimalSeparator)
+    if doubleStringValue.characterAt(0) == Character(decimalSeparator) {
+      doubleStringValue.insert("0", at: doubleStringValue.startIndex)
     }
     
-    public func unformattedText(from formatted: String?) -> String? {
-        guard let formattedString = formatted else { return nil }
-        
-        let unformattedString = formattedString
-            .replacingOccurrences(of: suffixStr ?? "", with: "")
-            .replacingOccurrences(of: prefixStr ?? "", with: "")
-            .replacingOccurrences(of: " ", with: "")
-            .replacingOccurrences(of: groupingSeparator, with: "")
-        
-        return unformattedString
+    if doubleStringValue.characterAt(doubleStringValue.count - 1) == Character(decimalSeparator) {
+      doubleStringValue.append("0")
+      hasIncompletedDecimal = true
     }
     
-    private func correctedDecimalPart(from string: String) -> String {
-        if string.count <= maximumDecimalCharacters { return string }
-        var tmpString = string
-        let numberOfExcessSymbols = tmpString.count - maximumDecimalCharacters
-        tmpString.removeLast(numberOfExcessSymbols)
-        return tmpString
+    guard let doubleValue = Double(doubleStringValue) else { return unformattedString }
+    
+    let decimalValue = Int(doubleValue)
+    resultString.append(stringForDecimal(decimal: decimalValue))
+    
+    if doubleStringValue.contains(decimalSeparator) {
+      resultString.append(decimalSeparator)
+      if doubleStringValue.characterAt(doubleStringValue.count - 1) != Character(decimalSeparator) {
+        let floatString = doubleStringValue.components(separatedBy: decimalSeparator)[1]
+        resultString.append(correctedDecimalPart(from: floatString))
+        
+      }
     }
+    
+    if hasIncompletedDecimal { resultString.removeLast() }
+    resultString.append(suffixStr ?? "")
+    return resultString
+  }
+  
+  public func unformattedText(from formatted: String?) -> String? {
+    guard let formattedString = formatted else { return nil }
+    
+    let unformattedString = formattedString
+      .replacingOccurrences(of: suffixStr ?? "", with: "")
+      .replacingOccurrences(of: prefixStr ?? "", with: "")
+      .replacingOccurrences(of: " ", with: "")
+      .replacingOccurrences(of: groupingSeparator, with: "")
+    
+    return unformattedString
+  }
+  
+  private func correctedDecimalPart(from string: String) -> String {
+    if string.count <= maximumDecimalCharacters { return string }
+    var tmpString = string
+    let numberOfExcessSymbols = tmpString.count - maximumDecimalCharacters
+    tmpString.removeLast(numberOfExcessSymbols)
+    return tmpString
+  }
 }
---
+++ Tests/SumUSFormatTests.swift
@@ -0,0 +1,146 @@
+//
+//  SumUSFormatTests.swift
+//  AnyFormatKitTests
+//
+//  Created by BRANDERSTUDIO on 11/20/17.
+//  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
+//
+
+import XCTest
+import AnyFormatKit
+
+class SumUSFormatTests: XCTestCase {
+  let sumFormatter = SumTextFormatter(textPattern: "#,###.##")
+}
+
+// MARK: - Empty string tests
+extension SumUSFormatTests {
+  func testEmptyString() {
+    let initialString = ""
+    let expectedString = ""
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func testNilString() {
+    let initialString: String? = nil
+    let expectedString: String? = nil
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(String(describing: expectedString))")
+  }
+}
+
+// MARK: - Zero values tests
+extension SumUSFormatTests {
+  func testZeroDecimal() {
+    let initialString = "0"
+    let expectedString = "0"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test0comma0Formatting() {
+    let initialString = "0,0"
+    let expectedString = "0.0"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test0dot0Formatting() {
+    let initialString = "0.0"
+    let expectedString = "0.0"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test0comma00Formatting() {
+    let initialString = "0,00"
+    let expectedString = "0.00"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+}
+
+// MARK: - Correct values tests
+extension SumUSFormatTests {
+  func test1Formatting() {
+    let initialString = "1"
+    let expectedString = "1"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test12Formatting() {
+    let initialString = "12"
+    let expectedString = "12"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test123Formatting() {
+    let initialString = "123"
+    let expectedString = "123"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+}
+
+// MARK: - Thousand values tests
+extension SumUSFormatTests {
+  func test1234Formatting() {
+    let initialString = "1234"
+    let expectedString = "1,234"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test1234dot5Formatting() {
+    let initialString = "1234.5"
+    let expectedString = "1,234.5"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test1234dot56Formatting() {
+    let initialString = "1234.56"
+    let expectedString = "1,234.56"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test123456Formatting() {
+    let initialString = "123456"
+    let expectedString = "123,456"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test1234567Formatting() {
+    let initialString = "1234567"
+    let expectedString = "1,234,567"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+  
+  func test1234567dot12Formatting() {
+    let initialString = "1234567.12"
+    let expectedString = "1,234,567.12"
+    let formattedString = sumFormatter.formattedText(from: initialString)
+    XCTAssert(expectedString == formattedString,
+              "\(String(describing: formattedString)) must be equal to \(expectedString)")
+  }
+}
