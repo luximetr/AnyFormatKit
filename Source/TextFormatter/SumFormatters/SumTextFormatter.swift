6779b39c58303d653d285e3c6ce54e2463603d9a
Aleksandr Orlov <alexandr.orlov@brander.ua>
2019-05-28 10:33:15 +0300

    Added negative prefix. Returned SumUSFormat tests, NegativeSumFormat tests, SumFormatBoundary tests.

--- Source/TextFormatter/SumFormatters/SumTextFormatter.swift
+++ Source/TextFormatter/SumFormatters/SumTextFormatter.swift
@@ -12,6 +12,13 @@ open class SumTextFormatter: TextFormatterProtocol {
   
   private let numberFormatter: NumberFormatter
   
+  open var maximumIntegerCharacters: Int = 14 {
+    didSet { numberFormatter.maximumIntegerDigits = maximumIntegerCharacters }
+  }
+  open var maximumDecimalCharacters: Int {
+    return numberFormatter.maximumFractionDigits
+  }
+  
   public init(numberFormatter: NumberFormatter) {
     self.numberFormatter = numberFormatter
   }
@@ -29,18 +36,40 @@ open class SumTextFormatter: TextFormatterProtocol {
     numberFormatter.minimumIntegerDigits = 1
     numberFormatter.allowsFloats = true
     numberFormatter.usesGroupingSeparator = true
+    numberFormatter.roundingMode = .down
+    numberFormatter.negativePrefix = "-"
     self.numberFormatter = numberFormatter
   }
+  
   
   open func formattedText(from unformatted: String?) -> String? {
     guard let unformatted = unformatted else { return nil }
     guard !unformatted.isEmpty else { return "" }
+    guard unformatted != negativePrefix else { return negativePrefix }
     let correctedUnformatted = unformatted.replacingOccurrences(of: ",", with: unformattedDecimalSeparator)
     let number = NSDecimalNumber(string: correctedUnformatted)
+    numberFormatter.minimumFractionDigits = calculateMinimumFractionDigits(unformatted: correctedUnformatted, divider: ".", maximumFractionDigits: numberFormatter.maximumFractionDigits)
     return numberFormatter.string(from: number)
   }
   
+  private var decimalSeparator: String {
+    return numberFormatter.decimalSeparator
+  }
+  
+  private func calculateMinimumFractionDigits(unformatted: String, divider: Character, maximumFractionDigits: Int) -> Int {
+    let currentFractionDigitsCount = getFractionDigitsCount(unformatted: unformatted, divider: divider)
+    if currentFractionDigitsCount >= maximumFractionDigits { return maximumFractionDigits }
+    return currentFractionDigitsCount
+  }
+  
+  private func getFractionDigitsCount(unformatted: String, divider: Character) -> Int {
+    let parts = unformatted.split(separator: divider)
+    guard parts.count > 1 else { return 0 }
+    return parts[1].count
+  }
+  
   private let unformattedDecimalSeparator = "."
+  private let negativePrefix = "-"
   private var unformattedNumberFormatter: NumberFormatter = {
     let numberFormatter = NumberFormatter()
     numberFormatter.allowsFloats = true
