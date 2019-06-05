84148624b1b5d9a35193ba8c0b55659b7f76ad7d
Aleksandr Orlov <alexandr.orlov@brander.ua>
2019-06-05 22:04:46 +0300

    Removed text input view.

--- Source/TextInput/TextInputView/TextInputView.swift
+++ Source/TextInput/TextInputView/TextInputView.swift
@@ -1,70 +1,70 @@
+////
+////  InputTextView.swift
+////  TextInput
+////
+////  Created by BRANDERSTUDIO on 09.10.2017.
+////  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
+////
 //
-//  InputTextView.swift
-//  TextInput
+//import UIKit
 //
-//  Created by BRANDERSTUDIO on 09.10.2017.
-//  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
+//open class TextInputView: AttributedTextInputView, TextInput {
+//  // MARK: - TextInput
+//  open var content: String? {
+//    set { super.text = newValue }
+//    get { return super.text }
+//  }
+//  
+//  open var attributedContent: NSAttributedString? {
+//    set { super.attributedText = newValue }
+//    get { return super.attributedText }
+//  }
+//  
+//  private(set) open var textInputDelegates = MulticastDelegate<TextInputDelegate>()
+//  
+//  /// multicast delegate for TextInputField delegate methods
+//  private(set) open var textInputViewDelegates = MulticastDelegate<TextInputViewDelegate>()
+//  
+//  // MARK: - Unavailable fields
+//  @available(*, unavailable, message: "use content instead")
+//  open override var text: String! {
+//    set {}
+//    get { return nil }
+//  }
+//  
+//  @available(*, unavailable, message: "use attributedContent instead")
+//  open override var attributedText: NSAttributedString! {
+//    set {}
+//    get { return nil }
+//  }
+//  
+////  @available(*, unavailable, message: "use textInputDelegates and inputTextViewDelegates instead")
+//  open override var delegate: UITextViewDelegate? {
+//    set { super.delegate = newValue }
+//    get { return super.delegate }
+//  }
+//  
+//  // MARK: - Init
+//  public override init(frame: CGRect, textContainer: NSTextContainer?) {
+//    super.init(frame: frame, textContainer: textContainer)
+//    initConfigure()
+//  }
+//  
+//  public required init?(coder aDecoder: NSCoder) {
+//    super.init(coder: aDecoder)
+//    initConfigure()
+//  }
+//}
 //
-
-import UIKit
-
-open class TextInputView: AttributedTextInputView, TextInput {
-  // MARK: - TextInput
-  open var content: String? {
-    set { super.text = newValue }
-    get { return super.text }
-  }
-  
-  open var attributedContent: NSAttributedString? {
-    set { super.attributedText = newValue }
-    get { return super.attributedText }
-  }
-  
-  private(set) open var textInputDelegates = MulticastDelegate<TextInputDelegate>()
-  
-  /// multicast delegate for TextInputField delegate methods
-  private(set) open var textInputViewDelegates = MulticastDelegate<TextInputViewDelegate>()
-  
-  // MARK: - Unavailable fields
-  @available(*, unavailable, message: "use content instead")
-  open override var text: String! {
-    set {}
-    get { return nil }
-  }
-  
-  @available(*, unavailable, message: "use attributedContent instead")
-  open override var attributedText: NSAttributedString! {
-    set {}
-    get { return nil }
-  }
-  
-//  @available(*, unavailable, message: "use textInputDelegates and inputTextViewDelegates instead")
-  open override var delegate: UITextViewDelegate? {
-    set { super.delegate = newValue }
-    get { return super.delegate }
-  }
-  
-  // MARK: - Init
-  public override init(frame: CGRect, textContainer: NSTextContainer?) {
-    super.init(frame: frame, textContainer: textContainer)
-    initConfigure()
-  }
-  
-  public required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    initConfigure()
-  }
-}
-
-// MARK: - InitConfigure
-private extension TextInputView {
-  /// Init configure for text view
-  func initConfigure() {
-    setupDelegate()
-  }
-  
-  /// Delegate is self for intercept delegate methods and transform it to TextInput delegate methods call
-  func setupDelegate() {
-    super.delegate = self
-  }
-}
+//// MARK: - InitConfigure
+//private extension TextInputView {
+//  /// Init configure for text view
+//  func initConfigure() {
+//    setupDelegate()
+//  }
+//  
+//  /// Delegate is self for intercept delegate methods and transform it to TextInput delegate methods call
+//  func setupDelegate() {
+//    super.delegate = self
+//  }
+//}
--- Source/TextInput/TextInputView/TextInputViewDelegateCall.swift
+++ Source/TextInput/TextInputView/TextInputViewDelegateCall.swift
@@ -1,100 +1,100 @@
+////
+////  TextInputViewDelegateCall.swift
+////  TextInput
+////
+////  Created by BRANDERSTUDIO on 13.10.2017.
+////  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
+////
 //
-//  TextInputViewDelegateCall.swift
-//  TextInput
+//import UIKit
 //
-//  Created by BRANDERSTUDIO on 13.10.2017.
-//  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
-//
-
-import UIKit
-
-/// Calling methods of textInputDelegates and textInputViewDelegates with UITextViewDelegate's methods
-extension TextInputView: UITextViewDelegate {
-  // MARK: - TextInputDelegate
-  open func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
-    guard let textInput = textView as? TextInputView else { return true }
-    var shouldBegin = true
-    textInputDelegates.invoke { delegate in
-      shouldBegin = delegate.textInputShouldBeginEditing(textInput) && shouldBegin
-    }
-    return shouldBegin
-  }
-  
-  open func textViewDidBeginEditing(_ textView: UITextView) {
-    guard let textInput = textView as? TextInputView else { return }
-    textInputDelegates.invoke { delegate in
-      delegate.textInputDidBeginEditing(textInput)
-    }
-  }
-  
-  open func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
-    guard let textInput = textView as? TextInputView else { return true }
-    var shouldEnd = true
-    textInputDelegates.invoke { delegate in
-      shouldEnd = delegate.textInputShouldEndEditing(textInput) && shouldEnd
-    }
-    return shouldEnd
-  }
-  
-  open func textViewDidEndEditing(_ textView: UITextView) {
-    guard let textInput = textView as? TextInputView else { return }
-    textInputDelegates.invoke { delegate in
-      delegate.textInputDidEndEditing(textInput)
-    }
-  }
-  
-  open func textView(_ textView: UITextView,
-                shouldChangeTextIn range: NSRange,
-                replacementText text: String) -> Bool {
-    guard let textInput = textView as? TextInputView else { return true }
-    var shouldChange = true
-    textInputDelegates.invoke { delegate in
-      shouldChange = delegate.textInput(
-        textInput, shouldChangeTextIn: range, replacementText: text) && shouldChange
-    }
-    return shouldChange
-  }
-  
-  // MARK: - TextInputViewDelegate
-  open func textViewDidChange(_ textView: UITextView) {
-    guard let textInput = textView as? TextInputView else { return }
-    textInputViewDelegates.invoke { delegate in
-      delegate.textInputDidChange(textInput)
-    }
-  }
-  
-  open func textViewDidChangeSelection(_ textView: UITextView) {
-    guard let textInput = textView as? TextInputView else { return }
-    textInputViewDelegates.invoke { delegate in
-      delegate.textInputDidChangeSelection(textInput)
-    }
-  }
-  
-  @available(iOS 10.0, *)
-  open func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-    guard let textInput = textView as? TextInputView else { return true }
-    var shouldInteract = true
-    textInputViewDelegates.invoke { delegate in
-      shouldInteract = delegate.textInput(
-        textInput,
-        shouldInteractWith: textAttachment,
-        in: characterRange,
-        interaction: interaction) && shouldInteract
-    }
-    return shouldInteract
-  }
-  
-  @available(iOS 10.0, *)
-  open func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-    guard let textInput = textView as? TextInputView else { return true }
-    var shouldInteract = true
-    textInputViewDelegates.invoke { delegate in
-      shouldInteract = delegate.textInput(
-        textInput,
-        shouldInteractWith: URL,
-        in: characterRange,
-        interaction: interaction) && shouldInteract
-    }
-    return shouldInteract
-  }
-}
+///// Calling methods of textInputDelegates and textInputViewDelegates with UITextViewDelegate's methods
+//extension TextInputView: UITextViewDelegate {
+//  // MARK: - TextInputDelegate
+//  open func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+//    guard let textInput = textView as? TextInputView else { return true }
+//    var shouldBegin = true
+//    textInputDelegates.invoke { delegate in
+//      shouldBegin = delegate.textInputShouldBeginEditing(textInput) && shouldBegin
+//    }
+//    return shouldBegin
+//  }
+//  
+//  open func textViewDidBeginEditing(_ textView: UITextView) {
+//    guard let textInput = textView as? TextInputView else { return }
+//    textInputDelegates.invoke { delegate in
+//      delegate.textInputDidBeginEditing(textInput)
+//    }
+//  }
+//  
+//  open func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+//    guard let textInput = textView as? TextInputView else { return true }
+//    var shouldEnd = true
+//    textInputDelegates.invoke { delegate in
+//      shouldEnd = delegate.textInputShouldEndEditing(textInput) && shouldEnd
+//    }
+//    return shouldEnd
+//  }
+//  
+//  open func textViewDidEndEditing(_ textView: UITextView) {
+//    guard let textInput = textView as? TextInputView else { return }
+//    textInputDelegates.invoke { delegate in
+//      delegate.textInputDidEndEditing(textInput)
+//    }
+//  }
+//  
+//  open func textView(_ textView: UITextView,
+//                shouldChangeTextIn range: NSRange,
+//                replacementText text: String) -> Bool {
+//    guard let textInput = textView as? TextInputView else { return true }
+//    var shouldChange = true
+//    textInputDelegates.invoke { delegate in
+//      shouldChange = delegate.textInput(
+//        textInput, shouldChangeTextIn: range, replacementText: text) && shouldChange
+//    }
+//    return shouldChange
+//  }
+//  
+//  // MARK: - TextInputViewDelegate
+//  open func textViewDidChange(_ textView: UITextView) {
+//    guard let textInput = textView as? TextInputView else { return }
+//    textInputViewDelegates.invoke { delegate in
+//      delegate.textInputDidChange(textInput)
+//    }
+//  }
+//  
+//  open func textViewDidChangeSelection(_ textView: UITextView) {
+//    guard let textInput = textView as? TextInputView else { return }
+//    textInputViewDelegates.invoke { delegate in
+//      delegate.textInputDidChangeSelection(textInput)
+//    }
+//  }
+//  
+//  @available(iOS 10.0, *)
+//  open func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+//    guard let textInput = textView as? TextInputView else { return true }
+//    var shouldInteract = true
+//    textInputViewDelegates.invoke { delegate in
+//      shouldInteract = delegate.textInput(
+//        textInput,
+//        shouldInteractWith: textAttachment,
+//        in: characterRange,
+//        interaction: interaction) && shouldInteract
+//    }
+//    return shouldInteract
+//  }
+//  
+//  @available(iOS 10.0, *)
+//  open func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+//    guard let textInput = textView as? TextInputView else { return true }
+//    var shouldInteract = true
+//    textInputViewDelegates.invoke { delegate in
+//      shouldInteract = delegate.textInput(
+//        textInput,
+//        shouldInteractWith: URL,
+//        in: characterRange,
+//        interaction: interaction) && shouldInteract
+//    }
+//    return shouldInteract
+//  }
+//}
