2d59bd1b09d6a3e5a3ce431ea0e89a3bd17d9fb5
Aleksandr Orlov <alexandr.orlov@brander.ua>
2019-06-09 13:45:26 +0300

    Removed unused methods from String+Extension.

--- Source/Categories/String+Extension.swift
+++ Source/Categories/String+Extension.swift
@@ -10,22 +10,6 @@ import Foundation
 
 // MARK: - String extension
 extension String {
-  /**
-   Filter string with given regular expression
-   
-   - Parameters:
-     - regex: optional regular expression, that use for filter all symbols, that don't satisfy the RegEx
-   
-   - Returns: optional filtered string with symbols, that satify to RegEx
-  */
-  func filter(regex: String?) -> String {
-    guard let regex = regex else { return self }
-    let regexPredicate = NSPredicate(format: "SELF MATCHES %@", regex)
-    let filteredCharacters = filter { (character) -> Bool in
-      regexPredicate.evaluate(with: String(character))
-    }
-    return String(filteredCharacters)
-  }
   
   /**
    Find and return character in string by index
@@ -39,27 +23,6 @@ extension String {
   func characterAt(_ index: Int) -> Character? {
     guard index < count else { return nil }
     return self[self.index(self.startIndex, offsetBy: index)]
-  }
-  
-  var first: String {
-    return String(prefix(1))
-  }
-  
-  var last: String {
-    return String(suffix(1))
-  }
-  
-  var uppercaseFirst: String {
-    return first.uppercased() + String(dropFirst())
-  }
-  
-  func addingPercentEncodingForQuery() -> String? {
-    let allowed = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._* ")
-    return addingPercentEncoding(withAllowedCharacters: allowed)?.replacingOccurrences(of: " ", with: "+")
-  }
-  
-  func isOnlyDigits() -> Bool {
-    return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: self))
   }
   
   func slice(from: String, toString: String) -> String? {
