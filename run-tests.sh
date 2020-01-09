ec229188ccada601518701362397d782a31503f1
Oleksandr Orlov <luximetr@gmail.com>
2020-01-10 03:03:28 +0800

    Updated run-tests script with new simulators.

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xcodebuild -workspace AnyFormatKit.xcworkspace -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.1' test
+xcodebuild -workspace AnyFormatKit.xcworkspace -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.3' test
