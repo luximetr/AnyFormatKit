88005f98d660da6ad7b0ed8f14e4ed23a84966b6
Artyom Rudakov <two_dollars@icloud.com>
2020-01-13 18:06:18 +0200

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "AnyFormatKit",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "AnyFormatKit",
+            targets: ["AnyFormatKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "AnyFormatKit",
+            path: "Source"
+        ),
+        .testTarget(
+            name: "AnyFormatKitTests",
+            dependencies: ["AnyFormatKit"],
+            path: "Tests"
+        )
+    ]
+)
