f16b7e7f98e0f67e87b7c9791dfe7bd138c1192e
aleksandr.orlov <aleksandr.orlov@deskera.com>
2020-11-13 13:07:06 +0200

    Added PlaceholderTextInputFormatterErasingBy1SymbolTests

--- Source/TextFormatter/PlaceholderFormatters/PlaceholderTextInputFormatter.swift
+++ Source/TextFormatter/PlaceholderFormatters/PlaceholderTextInputFormatter.swift
@@ -39,7 +39,7 @@ open class PlaceholderTextInputFormatter: TextInputFormatter {
   // MARK: - Format input
   
   public func formatInput(currentText: String, range: NSRange, replacementString text: String) -> FormattedTextValue {
-    let unformattedRange = self.unformattedRange(from: range)
+    let unformattedRange = self.unformattedRange(currentText: currentText, from: range)
     let oldUnformattedText = (textFormatter.unformat(currentText) ?? "") as NSString
     let newText = oldUnformattedText.replacingCharacters(in: unformattedRange, with: text)
     let formattedText = textFormatter.format(newText) ?? ""
@@ -47,12 +47,14 @@ open class PlaceholderTextInputFormatter: TextInputFormatter {
     return FormattedTextValue(formattedText: formattedText, caretBeginOffset: caretOffset)
   }
   
-  private func unformattedRange(from range: NSRange) -> NSRange {
+  private func unformattedRange(currentText: String, from range: NSRange) -> NSRange {
+    guard currentText != textPattern else { return NSRange(location: 0, length: 0) }
     let newRange = NSRange(
       location: range.location - textPattern[..<textPattern.index(textPattern.startIndex, offsetBy: range.location)]
         .replacingOccurrences(of: String(patternSymbol), with: "").count,
       length: range.length - (textPattern as NSString).substring(with: range)
-        .replacingOccurrences(of: String(patternSymbol), with: "").count)
+        .replacingOccurrences(of: String(patternSymbol), with: "").count
+    )
     return newRange
   }
   
