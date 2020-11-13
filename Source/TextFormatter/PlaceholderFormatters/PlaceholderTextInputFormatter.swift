f73850e1a3068100e9873204af948f0f4ff83bfb
aleksandr.orlov <aleksandr.orlov@deskera.com>
2020-11-13 16:36:10 +0200

    Added PlaceholderTextInputFormatterReplace3SymbolsTests

--- Source/TextFormatter/PlaceholderFormatters/PlaceholderTextInputFormatter.swift
+++ Source/TextFormatter/PlaceholderFormatters/PlaceholderTextInputFormatter.swift
@@ -39,8 +39,8 @@ open class PlaceholderTextInputFormatter: TextInputFormatter {
   // MARK: - Format input
   
   public func formatInput(currentText: String, range: NSRange, replacementString text: String) -> FormattedTextValue {
-    let unformattedRange = self.unformattedRange(currentText: currentText, from: range)
     let oldUnformattedText = (textFormatter.unformat(currentText) ?? "") as NSString
+    let unformattedRange = self.unformattedRange(currentText: currentText, from: range)
     let newText = oldUnformattedText.replacingCharacters(in: unformattedRange, with: text)
     let formattedText = textFormatter.format(newText) ?? ""
     let caretOffset = getCorrectedCaretPosition(range: range, replacementString: text)
@@ -48,14 +48,51 @@ open class PlaceholderTextInputFormatter: TextInputFormatter {
   }
   
   private func unformattedRange(currentText: String, from range: NSRange) -> NSRange {
-    guard currentText != textPattern else { return NSRange(location: 0, length: 0) }
-    let newRange = NSRange(
-      location: range.location - textPattern[..<textPattern.index(textPattern.startIndex, offsetBy: range.location)]
-        .replacingOccurrences(of: String(patternSymbol), with: "").count,
-      length: range.length - (textPattern as NSString).substring(with: range)
-        .replacingOccurrences(of: String(patternSymbol), with: "").count
-    )
-    return newRange
+    var location = 0
+    var length = 0
+    let currentTextInRange = (currentText as NSString).substring(with: range)
+    if let currentTextChar = currentTextInRange.characterAt(0),
+       currentTextChar == patternSymbol {
+      let currentUnformattedText = textFormatter.unformat(currentText) ?? ""
+      location = currentUnformattedText.count
+    } else {
+      location = range.location - getNumberOfFormatChars(text: currentText, before: range.location)
+    }
+    length = range.length - getNumberOfFormChars(in: currentText, textPattern: textPattern, range: range)
+    return NSRange(location: location, length: length)
+  }
+  
+  private func getNumberOfFormatChars(text: String, before: Int) -> Int {
+    let textLeftSlice = text.leftSlice(limit: before)
+    var result = 0
+    for charIndex in 0..<textLeftSlice.count {
+      if getIsFormatCharacter(text: textLeftSlice, textPattern: textPattern, index: charIndex) {
+        result += 1
+      }
+    }
+    return result
+  }
+  
+  private func getNumberOfFormChars(in text: String, textPattern: String, range: NSRange) -> Int {
+    let textInRange = (text as NSString).substring(with: range)
+    let textPatternInRange = (textPattern as NSString).substring(with: range)
+    var result = 0
+    for index in 0..<textInRange.count {
+      guard let textChar = textInRange.characterAt(index) else { continue }
+      guard let textPatternChar = textPatternInRange.characterAt(index) else { continue }
+      if textChar == textPatternChar {
+        result += 1
+      }
+    }
+    return result
+  }
+  
+  private func getIsFormatCharacter(text: String, textPattern: String, index: Int) -> Bool {
+    guard let textChar = text.characterAt(index),
+          let textPatternChar = textPattern.characterAt(index) else {
+      return false
+    }
+    return textChar == textPatternChar
   }
   
   // MARK: - Caret position calculation
