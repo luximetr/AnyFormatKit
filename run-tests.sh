9eca37f181500cbdbe0a0fe4684818d6da840dca
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-01-28 18:54:23 +0200

    Changed simulator name in run-tests script.

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xcodebuild -project AnyFormatKit.xcodeproj -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12 Pro Max,OS=14.3' test
+xcodebuild -project AnyFormatKit.xcodeproj -scheme "AnyFormatKit" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.3' test
