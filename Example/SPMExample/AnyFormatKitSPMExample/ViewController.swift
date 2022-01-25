c26cd10da10f9ad297e706491e9d676ddf29bab7
aleksandr.orlov <aleksandr.orlov@deskera.com>
2022-01-25 22:10:18 +0800

    Moved example project to separate folder. Created example project for SPM.

---
+++ Example/SPMExample/AnyFormatKitSPMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnyFormatKitSPMExample
+//
+//  Created by Oleksandr Orlov on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
